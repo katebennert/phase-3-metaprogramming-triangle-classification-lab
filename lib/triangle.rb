e39215bd8eb541f3f9fe449310b3c8148d749408
Kate Bennert <kate.bennert@gmail.com>
2023-03-31 09:03:27 -0400

    commit tree 76c4660d1bb1f30e9b2fa6c5b8fd480e85a051fa

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,38 @@
 class Triangle
-  # write code here
+  attr_accessor :s1, :s2, :s3
+
+  def initialize(s1, s2, s3)
+    @s1 = s1
+    @s2 = s2
+    @s3 = s3
+  end
+
+  def kind()
+    raise TriangleError unless valid_sides? && valid_triangle?
+
+    if s1 == s2 && s2 == s3
+      :equilateral
+    elsif s1 == s2 || s2 == s3 || s3 == s1
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  def valid_sides?
+    s1 > 0 && s2 > 0 && s3 > 0
+  end
+
+  def valid_triangle?
+    s1 + s2 > s3 && s2 + s3 > s1 && s3 + s1 > s2
+  end
+  
+
+    class TriangleError < StandardError
+      def message
+        "You must input a valid triangle!"
+      end
+   end
+   
+
 end
